e4c38d0a7a48dc0203959c84bf4e238329a420bf
Mattias Buelens <mattias@buelens.com>
2024-04-14 17:18:05 +0200

    Set up syntax highlighting

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import "reveal.js/dist/reveal.css";
 import "reveal.js/dist/theme/moon.css";
+import "highlight.js/styles/night-owl.css";
 import "./style.css";
 import Reveal from "reveal.js";
+import "highlight.js";
+import highlightJavascript from "highlight.js/lib/languages/javascript";
+import RevealHighlight from "reveal.js/plugin/highlight/highlight";
 import RevealNotes from "reveal.js/plugin/notes/notes";
 
 const deck = new Reveal({
-  plugins: [RevealNotes],
+  plugins: [RevealHighlight, RevealNotes],
+  highlight: {
+    beforeHighlight: (hljs) =>
+      hljs.registerLanguage("javascript", highlightJavascript),
+  },
 });
 deck.initialize({ hash: true, slideNumber: true });
