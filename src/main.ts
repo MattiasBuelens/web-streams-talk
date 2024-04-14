a66b68c75caafbb79555f4fee8c65eec70bac4c9
Mattias Buelens <mattias@buelens.com>
2024-04-14 16:53:50 +0200

    Set up speaker notes

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import "reveal.js/dist/reveal.css";
 import "reveal.js/dist/theme/dracula.css";
 import "./style.css";
 import Reveal from "reveal.js";
+import RevealNotes from "reveal.js/plugin/notes/notes";
 
-const deck = new Reveal();
+const deck = new Reveal({
+  plugins: [RevealNotes],
+});
 deck.initialize({ hash: true, slideNumber: true });
