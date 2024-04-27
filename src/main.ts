b8edfef81b7388fa5459372806d4a4b336be759c
Mattias Buelens <mattias@buelens.com>
2024-04-27 17:59:19 +0200

    Explain sync/async value(s)

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,26 @@ import "highlight.js/styles/night-owl.css";
 import "./style.css";
 import Reveal from "reveal.js";
 import "highlight.js";
-import highlightJavascript from "highlight.js/lib/languages/javascript";
+import highlightJavaScript from "highlight.js/lib/languages/javascript";
+import highlightTypeScript from "highlight.js/lib/languages/typescript";
 import RevealHighlight from "reveal.js/plugin/highlight/highlight";
 import RevealNotes from "reveal.js/plugin/notes/notes";
 
 const deck = new Reveal({
   plugins: [RevealHighlight, RevealNotes],
   highlight: {
-    beforeHighlight: (hljs) =>
-      hljs.registerLanguage("javascript", highlightJavascript),
+    highlightOnLoad: false,
+    beforeHighlight: (hljs) => {
+      hljs.registerLanguage("javascript", highlightJavaScript);
+      hljs.registerLanguage("typescript", highlightTypeScript);
+    },
   },
 });
 deck.initialize({ hash: true, slideNumber: true });
+
+const highlight = deck.getPlugin("highlight") as any;
+for (const code of document.querySelectorAll(
+  '.reveal code[class^="language-"]',
+)) {
+  highlight.highlightBlock(code);
+}
