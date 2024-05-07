40e99a9f76a753c0f4fdc0e25aceb1ba5181d50e
Mattias Buelens <mattias@buelens.com>
2024-05-07 16:11:14 +0200

    Fix base URL

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
+import process from "node:process";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: process.env.VITE_BASE_URL || "",
   css: {
     devSourcemap: true,
   },
